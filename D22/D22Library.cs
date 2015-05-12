e99bafd41aff84ca57d4a12dfd84f134cd7ff706
fishboi <fishboi258789@gmail.com>
2015-05-12 09:27:16 -0500

    Commit initial directory

---
+++ D22/D22Library.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace D22
+{
+	public static class Initializer
+	{
+		public static void Initialize()
+		{
+			Console.WriteLine ("Initializing...");
+			Console.WriteLine ("Done!");
+		}
+	}
+
+	public class PlayableCharacter
+	{
+		public string Name;
+
+		public PlayableCharacter (string name)
+		{
+			Name = name;
+		}
+	}
+}
---
+++ D22/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace D22
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Initializer.Initialize ();
+		}
+	}
+}
